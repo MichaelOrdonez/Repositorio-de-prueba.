a21f288ba7e74cdbab2bd1fe3b8e4261b63bbe32
MichaelOrdonez <mikael.oie13@gmail.com>
2025-03-25 21:08:29 -0500

    Add files via upload

---
+++ Sin título7.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 25 10:40:29 2025
+
+@author: lab
+"""
+
+for numero in range(1,11):
+    print(numero)
+    
+for numero in range(10,0,-1):
+    print(numero)
+    
